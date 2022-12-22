f67d197ab40fd62a82105243557560352489f460
Davi Luiz <davilucena222@gmail.com>
2022-12-22 01:42:23 +0000

    Finishing profile details

--- src/pages/Blog/components/profile/index.tsx
+++ src/pages/Blog/components/profile/index.tsx
@@ -1,9 +1,40 @@
-import { ProfileContainer, ProfilePicture } from "./styles";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faBuilding, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ExternalLink } from "../../../../components/ExternalLink";
+import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles";
 
 export function Profile() {
   return (
     <ProfileContainer>
       <ProfilePicture src="https://github.com/davilucena222.png" />
+
+      <ProfileDetails>
+        <header>
+          <h1>Davi Luiz</h1>
+
+          <ExternalLink text={"GitHub"} href="#" />
+        </header>
+
+        <p>
+          Hi, I am Davi Luiz. I am a Front-End Software Engineer and currently I am working on projects to help impact the community tech and build applications to help other people that are trying to migrate your career.
+        </p>
+
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={faGithub} />
+            davilucena222
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBuilding} />
+            Google
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faUserGroup} />
+            15 seguidores
+          </li>
+        </ul>
+      </ProfileDetails>
     </ProfileContainer>
   );
 }
--- src/pages/Blog/components/profile/styles.ts
+++ src/pages/Blog/components/profile/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const ProfileContainer = styled.section`
   width: 100%;
   min-height: 13.25rem;
+  margin-top: -5.5rem;
   background: ${({ theme }) => theme.colors["base-profile"]};
   box-shadow: 0px 2px 20px rgba(0, 0, 0, 0.2);
   border-radius: 10px;
@@ -16,4 +17,43 @@ export const ProfilePicture = styled.img`
   height: 9.25rem;
   border-radius: 8px;
   object-fit: cover;
+`;
+
+export const ProfileDetails = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 0.5rem;
+
+    h1 {
+      font-size: ${({ theme }) => theme.textSizes["title-title-l"]};
+      color: ${({ theme }) => theme.colors["base-title"]};
+      line-height: 130%;
+    }
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+    flex-wrap: wrap;
+    margin-top: auto;
+
+    li {
+      display:  flex;
+      align-items: center;
+      gap: 0.5rem;
+
+      svg {
+        width: 1.125rem;
+        height: 1.125rem;
+        color: ${({ theme }) => theme.colors["base-label"]};
+      }
+    }
+  }
 `;

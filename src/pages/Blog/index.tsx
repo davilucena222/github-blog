8b4a288c0205c0a081ba038e6a400cfd65f195d9
Davi Luiz <davilucena222@gmail.com>
2022-12-22 12:57:42 +0000

    editing post list

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,23 @@
 import { SearchInput } from "../../components/SearchInput";
+import { Post } from "./components/post";
 import { Profile } from "./components/profile";
+import { PostListContainer } from "./styles";
 
 export function Blog() {
   return (
     <>
       <Profile />
       <SearchInput />
+
+      <PostListContainer>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </PostListContainer>
     </>
   );
 }
